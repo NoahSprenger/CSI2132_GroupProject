7e034038a72ff96e6c220be0782ecca402dabcf4
sattar <sabdu125@uottawa.ca>
2023-04-11 00:00:07 -0400

    updated sql files

---
+++ Deliverable2/sql_Files/hotel_room.sql
@@ -0,0 +1,31 @@
+-- Table: public.hotel_room
+
+-- DROP TABLE IF EXISTS public.hotel_room;
+
+CREATE TABLE IF NOT EXISTS public.hotel_room
+(
+    price integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    views text COLLATE pg_catalog."default" NOT NULL,
+    extendable boolean NOT NULL,
+    damages text COLLATE pg_catalog."default",
+    "room_ID" integer NOT NULL,
+    "hotel_ID" integer NOT NULL,
+    capacity text COLLATE pg_catalog."default" NOT NULL,
+    amenities text[] COLLATE pg_catalog."default",
+    room_status ENUM ('AVAILABLE', 'UNAVAILABLE') DEFAULT 'AVAILABLE',
+    CONSTRAINT "Hotel_Room_pkey" PRIMARY KEY ("room_ID"),
+    CONSTRAINT price UNIQUE (price),
+    CONSTRAINT room_id UNIQUE ("room_ID"),
+    CONSTRAINT "hotel_ID" FOREIGN KEY ("hotel_ID")
+        REFERENCES public.hotel ("hotelID") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID,
+    CONSTRAINT views CHECK (views = ANY (ARRAY['Mountain'::character varying::text, 'Ocean'::character varying::text, 'City'::character varying::text])) NOT VALID,
+    CONSTRAINT capacity CHECK (capacity = ANY (ARRAY['single'::character varying::text, 'double'::character varying::text, 'triple'::character varying::text])) NOT VALID
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.hotel_room
+    OWNER to postgres;
